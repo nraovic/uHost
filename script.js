e1b20a269f6f948c85c9480e810b888dc1df37cf
nraovic <n.raovic@gmail.com>
2018-09-25 22:23:41 +0200

    Create mobile navigation and toggle button for it

--- script.js
+++ script.js
@@ -2,25 +2,35 @@ const backdrop = document.getElementById('backdrop');
 const plans = document.querySelectorAll('.plan');
 const modal = document.getElementById('modal');
 const noButton = document.querySelector('.modal__action--negative');
+const toggleButton = document.querySelector('.toggle-button');
+const mobileNav = document.querySelector('.mobile-nav');
 
 const addModalAndBackdrop = () => {
-  backdrop.style.display = 'block';
-  modal.style.display = 'block';
+  backdrop.classList.add('open');
+  modal.classList.add('open');
 };
-for (let node of plans) {
-  node.onclick = () => {
+for (let plan of plans) {
+  plan.onclick = () => {
     addModalAndBackdrop();
   };
 }
 
 //Close modal and backdrop when No button in modal pressed
 const closeModalandBackdrop = () => {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  backdrop.classList.remove('open');
+  modal.classList.remove('open');
+  mobileNav.classList.remove('open');
 };
 noButton.onclick = () => {
   closeModalandBackdrop();
 };
 backdrop.onclick = () => {
   closeModalandBackdrop();
+};
+
+//Toggle Navigation
+
+toggleButton.onclick = () => {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
 };
